6af458453dc5b318cf6ef5e5dfbc6fc5ee976df9
Twanghofer <tobias.w01@gmx.at>
2019-11-06 14:15:55 +0100

    created text and a symobl for the second part of the room, began scripting the flashlight

--- VR_Game_4Light/Assets/Game/Scripts/flashLightScript.cs
+++ VR_Game_4Light/Assets/Game/Scripts/flashLightScript.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class NewBehaviourScript : MonoBehaviour
+
+
+public class flashLightScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshProUGUI hiddenObject1;
+    public Image hiddenObject2;
+
+    void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.collider.tag == "showToggleObject1")
+        {
+            hiddenObject1.enabled = false;
+            Debug.Log("Object 1 is targetted");
+        }
+
+        if (collision.collider.tag == "showToggleObject2")
+        {
+            hiddenObject2.enabled = false;
+            Debug.Log("Object 2 is targetted");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }

3848cc4069fc2f962349834eddeb5a2023a3c26f
Twanghofer <tobias.w01@gmx.at>
2019-11-10 21:29:23 +0100

    added globe animation and animator that activates when completed

--- VR_Game_4Light/Assets/Game/Scripts/buildungScript.cs
+++ VR_Game_4Light/Assets/Game/Scripts/buildungScript.cs
@@ -7,11 +7,13 @@ public class buildungScript : MonoBehaviour
     public GameObject[] holoPosObj;
     public GameObject[] Objects;
 
-    float distanceObj0;
-    float distanceObj1;
-    float distanceObj2;
+    float distanceObj0 = 10;
+    float distanceObj1 = 10;
+    float distanceObj2 = 10;
 
     float snapDistance = 0.25F;
+
+    public Animation anim;
 
     void Update()
     {
@@ -22,6 +24,7 @@ public class buildungScript : MonoBehaviour
         if (distanceObj0 < snapDistance)
         {
             Objects[0].transform.position = holoPosObj[0].transform.position;
+            Objects[0].transform.rotation = holoPosObj[0].transform.rotation;
             Objects[0].GetComponent<Rigidbody>().isKinematic = true;
             Objects[0].GetComponent<Collider>().enabled = false;
             holoPosObj[0].SetActive(false);
@@ -30,18 +33,22 @@ public class buildungScript : MonoBehaviour
         if (distanceObj1 < snapDistance)
         {
             Objects[1].transform.position = holoPosObj[1].transform.position;
+            Objects[1].transform.rotation = holoPosObj[1].transform.rotation;
             Objects[1].GetComponent<Rigidbody>().isKinematic = true;
             Objects[1].GetComponent<Collider>().enabled = false;
             holoPosObj[1].SetActive(false);
+            anim.Play("Spin");
         }
 
         if (distanceObj2 < snapDistance)
         {
             Objects[2].transform.position = holoPosObj[2].transform.position;
+            Objects[2].transform.rotation = holoPosObj[0].transform.rotation;
             Objects[2].GetComponent<Rigidbody>().isKinematic = true;
             Objects[2].GetComponent<Collider>().enabled = false;
             holoPosObj[2].SetActive(false);
 
         }
     }
+    
 }
